2a1b7bd5e8437285f24869dc6087f9a2ae67dc86
josakko <josakko2358@gmail.com>
2024-04-28 23:40:48 +0200

    gave executable permissions to build.sh scripts for packaging

--- packaging/linux/build.sh
+++ packaging/linux/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Deb packaging
 docker build -t atlauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
 docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.4-1.deb
