5c7f1bea488e52e5ab83f8bef8b00d3ee1b59c0e
josakko <josakko2358@gmail.com>
2024-04-30 20:36:20 +0200

    update build.sh for linux packaging to include version in out file for deb

--- packaging/linux/build.sh
+++ packaging/linux/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+version="$(cat "../../src/main/resources/version")"
+
 # Deb packaging
 docker build -t atlauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
-docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.4-1.deb
+docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-$version.deb
 
 # RPM packaging
 docker build -t atlauncher/atlauncher-packaging-linux-rpm -f rpm/Dockerfile .
